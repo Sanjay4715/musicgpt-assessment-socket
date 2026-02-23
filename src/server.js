790684af9ab550549d8e39a61e1a4d17f235c8e7
Sanjaya Rai <sonjrai@gmail.com>
2026-02-23 10:34:46 +0545

    server busy events added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import { Server } from "socket.io";
 import { v4 } from "uuid";
-import { STATUS_TYPE, versions } from "./constants/index.js";
+import { files, STATUS_TYPE, thumbnails, versions } from "./constants/index.js";
 import { generateSongTitle, getRandomItem } from "./common/index.js";
 
 const server = http.createServer();
@@ -24,21 +24,42 @@ io.on("connection", (socket) => {
   // Send existing tasks to new client
   socket.emit("allTasks", tasks);
 
-  socket.on("submitPrompt", (prompt) => {
+  socket.on("submitPrompt", ({ prompt, id }) => {
+    const taskId = id ? id : v4();
+    const activeTasks = tasks.filter((t) => t.status !== STATUS_TYPE.SUCCESS);
+
+    console.log("activeTasks", activeTasks.length, activeTasks);
+
+    if (activeTasks.length >= 5) {
+      console.log(
+        "Server is busy. Please wait until one of the tasks completes.",
+      );
+      socket.emit("serverBusy", {
+        id: taskId,
+        prompt,
+        created_at: new Date().toISOString(),
+        status: STATUS_TYPE.FAILED,
+        error_tag: "SERVER_BUSY",
+        error_message: "Oops! Server busy",
+        error_message_detail:
+          "Many task or users on queue. Please wait for a moment until a task is complete",
+      });
+      return;
+    }
+
     const task = {
-      id: v4(),
+      id: taskId,
       status: STATUS_TYPE.CONNECTING,
       title: generateSongTitle(),
       created_at: new Date().toISOString(),
-      image_custom_thumbnail:
-        "https://lalals.s3.amazonaws.com/GenImages/faba14e1-ba20-4310-8ecd-3cbd9e70e58a_thumbnail.jpg",
+      image_custom_thumbnail: getRandomItem(thumbnails),
       version_string: getRandomItem(versions),
       input_prompt: prompt,
       progress: 0,
     };
 
     tasks.push(task);
-    console.log(`Prompt submitted: "${prompt}" -> Item ID: ${task.id}`);
+    console.log(`Prompt submitted: "${prompt}" -> Item ID: ${taskId}`);
 
     let index = 0;
     const interval = setInterval(() => {
@@ -74,7 +95,19 @@ io.on("connection", (socket) => {
         index++;
       } else {
         task.status = STATUS_TYPE.SUCCESS;
-        io.emit("finalData", { ...task });
+        const {
+          file_output_0,
+          lyrics,
+          lyrics_output_timestamped,
+          audio_length_ms,
+        } = getRandomItem(files);
+        io.emit("finalData", {
+          ...task,
+          file_output_0,
+          lyrics,
+          lyrics_output_timestamped,
+          audio_length_ms,
+        });
         console.log("Finished:", task);
         clearInterval(interval);
       }
